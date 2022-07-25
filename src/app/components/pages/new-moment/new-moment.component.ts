318fdf59d6257bdc185d8e8b5282d2fe8fea6a57
Jean Fernandine <jfernandine@gmail.com>
2022-07-25 13:11:28 -0300

    frontend moments

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,34 @@
+import { MomentService } from './../../../services/moment.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Moment } from 'src/app/Moments';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar';
+
+  constructor(private MomentService: MomentService) { }
+
+  ngOnInit(): void {
+  }
+
+  async createHandler(moment:Moment) {
+    const formData = new FormData()
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+    if(moment.image) {
+      formData.append('image', moment.image);
+    }
+
+
+    await this.MomentService.createMoment(formData).subscribe;
+
+
+  }
+
+}
